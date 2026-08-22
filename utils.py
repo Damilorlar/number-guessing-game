38389ee00d74e00bdc06d29623db329f0fd286d2
Jezreal Momoh <jezreelmomoh1234@gmail.com>
2026-08-22 02:52:12 +0100

    Still working on the difficulty function

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+#
 def get_valid_guess():
     while True:
         try:
@@ -5,3 +6,21 @@ def get_valid_guess():
             return guess
         except ValueError:
             print("That's not a valid number. Try again.")
+
+
+DIFFICULTIES = {
+    "easy": {"min": 1, "max": 100, "attempts": 10},
+    "medium": {"min": 1, "max": 150, "attempts": 7},
+    "hard": {"min": 1, "max": 200, "attempts": 5}
+}
+
+def get_difficulty():
+    print("Select difficulty: [1] Easy  [2] Medium  [3] Hard")
+    player_choice = input("> ")
+
+    if player_choice == "1":
+
+    elif player_choice == "2":
+
+    elif player_choice == "3":
+        
